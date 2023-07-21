3b7b89ffc45603146d33b7e7b64cd7ff387da5dd
astanelis <stanelisa@gmail.com>
2023-07-21 07:23:23 +0300

    pull

---
+++ brand/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './index.css'
+import Logo from './components/Logo'
+import Image from './components/Image'
+import Text from './components/Text'
+import Form from './components/Form'
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="brand">
+        <Logo />
+        <Image />
+        <Text />
+      </div>
+      <Form />
+    </div>
+  )
+}
+
+export default App
